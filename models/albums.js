3f64f152bc6ab789c875f3843cf69efcc6da5c49
nuranned <nuranned@gmail.com>
2017-05-30 15:45:23 -0700

    made database

--- models/albums.js
+++ models/albums.js
@@ -1,3 +1,12 @@
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema;
-  
+
+var AlbumsSchema = new Schema ({
+  artistName: String,
+  name: String,
+  releaseDate: String,
+  genres: [ String ]
+});
+
+var Album = mongoose.model('Album', AlbumsSchema)
+module.exports = Album;
